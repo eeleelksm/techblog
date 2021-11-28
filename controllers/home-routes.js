26406b9f4326ca90ba97f240d15f4f94a12cfbcc
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-28 12:16:17 -0500

    fixed posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  res.render('homepage', {
+    id: 1,
+    title: 'Handlebars Docs',
+    blog_text: "blah",
+    created_at: new Date(),
+    comments: [{}, {}],
+    user: {
+      username: 'test_user'
+    }
+  });
 });
 
 module.exports = router;
