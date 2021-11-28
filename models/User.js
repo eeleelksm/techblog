b613beb1cefb360c787097b38d6e7d633e3995bb
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-27 21:35:58 -0500

    user password login

--- models/User.js
+++ models/User.js
@@ -3,8 +3,10 @@ const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
 
 class User extends Model {
-
-}
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  };
+};
 
 // create fields/columns for the Users
 User.init (
