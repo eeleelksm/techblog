56707adaa820819e14a0cd01a72001bb57718b95
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-28 13:20:27 -0500

    all posts appearing

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,32 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
 
 router.get('/', (req, res) => {
-  res.render('homepage', {
-    id: 1,
-    title: 'Handlebars Docs',
-    blog_text: "blah",
-    created_at: new Date(),
-    comments: [{}, {}],
-    user: {
-      username: 'test_user'
-    }
+  Post.findAll({
+    attributes: ["id", "title", "blog_text", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ["username"]
+      }
+    ]
+  })
+  .then(dbPostData => {
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+    res.render('homepage', { posts });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
   });
 });
 
