aea32e63058295ad9e186c25d1c259826379cf5d
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-28 17:35:24 -0500

    user won't stay logged in'

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -61,7 +61,7 @@ router.post('/', (req, res) => {
   Post.create({
     title: req.body.title,
     blog_text: req.body.blog_text,
-    user_id: req.body.user_id
+    user_id: req.session.user_id
   })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
