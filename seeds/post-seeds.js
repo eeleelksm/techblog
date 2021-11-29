c4bb93075b0179a9a50ea4cde88b4316d526ee0a
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-11-28 21:42:35 -0500

    seeding

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,29 +2,29 @@ const { Post } = require('../models');
 
 const postData = [
     {
-        title: "Taskmaster goes public!",
-        post_content: "Taskinator is a task-tracker app that will allow you to organize your personal to-do list items by clicking and dragging them into categories like To Do, In Progress, and Completed.",
+        title: "Amazing GitHub Repositories",
+        post_text: "To lower the barrier to Software Development, Quincy Larson founded FreeCodeCamp in 2014. It offers a Virtual Code Camp so that the enrolled students can learn Software Development by working on projects in an interactive Platform.",
         user_id: 3
     },
     {
-        title: "Zoo Keepr reaches 1 million subscribers!",
-        post_content: "The local zoo has received funding to build a new online catalog, and they've asked to create a web server for a front-end application they’re developing, called Zoo Keepr. This site’s data will be stored on the server. This will allow animal enthusiasts to access the data from different locations and browsers without needing to download it to their device",
+        title: "Free Programming Books",
+        post_text: "Fortunately for us, there are many free books available on the internet for developers. This repository maintains the list of all free programming-related books. It also categorizes the books and lists the books in many major languages.",
         user_id: 1
     },
     {
-        title: "Work Day Scheduler tool now available!",
-        post_content: "A simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery. Use Moment.js library to work with date and time",
+        title: "System Design",
+        post_text: "Designing a Scalable end-to-end Software System (Frontend to Database) is still a very challenging task. A System Designer needs to know a particular topic deeply and should know a wide range of topics.",
         user_id: 2
 
     },
     {
-        title: "Tech Blog has been released!",
-        post_content: "A CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well. This site will be built from scratch and deployed to Heroku. The app will follow MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication",
-        user_id: 5
+        title: "What's Z Shell?",
+        post_text: "Z Shell (Zsh) is a popular extension of the Bourne shell (UNIX) and offers many improvements over the other standard shells (bash, csh). If used correctly, it can give a significant performance boost. ",
+        user_id: 1
     },
     {
-        title: "Just Tech News goes public!",
-        post_content: "Just Tech News—a tech news website where users can post, upvote, and comment on links to news articles. Use Sequelize, an object-relational mapping (ORM) library, to simplify your MySQL queries, add password hashing so that users can create secure passwords, and connect your application to JawsDB, a MySQL add-on for Heroku",
+        title: "The Art of the Command Line",
+        post_text: "One of the most effective but often neglected ways to improve productivity is to gain mastery in the command line. Initially, the UNIX-based systems mainly focused on Command-Line tools.",
         user_id: 4
     }
 ]
